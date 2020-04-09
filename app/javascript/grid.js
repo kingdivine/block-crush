2e308fe406b188196dee09a8bd4286fdf5d05dc7
kingdivine <divineanamekwe@gmail.com>
2020-04-09 19:59:07 +0100

    Initial commit

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -1,11 +1,12 @@
-'use strict';
+"use strict";
 
-const COLOURS = ['red', 'green', 'blue', 'yellow'];
+const COLOURS = ["#6b5b95", "#feb236", "#d64161", "#ff7b25"];
+const WHITE = ["white"];
 const MAX_X = 10;
 const MAX_Y = 10;
 
 class Block {
-  constructor (x, y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
@@ -14,7 +15,7 @@ class Block {
 }
 
 class BlockGrid {
-  constructor () {
+  constructor() {
     this.grid = [];
 
     for (let x = 0; x < MAX_X; x++) {
@@ -30,43 +31,98 @@ class BlockGrid {
   }
 
   redrawBlock(blockEl, block) {
-    const {x, y, colour} = block;
+    const { x, y, colour } = block;
     const id = `block_${x}x${y}`;
 
     blockEl.id = id;
-    blockEl.className = 'block';
+    blockEl.className = "block";
     blockEl.style.background = block.colour;
   }
-  getNeighbours(block){
-    /*
-    how do you get all neighbours of a given block given its coordinates
-    */
+
+  clearBlock(block) {
+    block.colour = WHITE;
   }
 
-  getSameColourNeighbours(block){
-    /*
-    how do you get only same color blocks?
-    */
-
+  removeBlock(block) {
+    block.colour = WHITE;
+    block.clickable = false;
   }
 
-  render(grid=document.querySelector('#gridEl')) {
+  getNeighbours(block) {
+    let neighbours = [];
+    const { x, y } = block;
+
+    x - 1 >= 0 ? neighbours.push(this.grid[x - 1][y]) : {};
+    x + 1 < MAX_X ? neighbours.push(this.grid[x + 1][y]) : {};
+    y - 1 >= 0 ? neighbours.push(this.grid[x][y - 1]) : {};
+    y + 1 < MAX_Y ? neighbours.push(this.grid[x][y + 1]) : {};
+
+    return neighbours.filter((neighbour) => neighbour.clickable === true);
+  }
+
+  getSameColourNeighbours(block) {
+    if (
+      this.getNeighbours(block).filter(
+        (neighbour) => neighbour.colour === block.colour
+      ).length === 0
+    ) {
+      return [];
+    }
+
+    let sameColourNeighbours = [];
+    let parent = this;
+    let ogColour = block.colour;
+
+    function recurse(block) {
+      parent.clearBlock(block);
+      sameColourNeighbours.push(block);
+      let neighbours = parent.getNeighbours(block);
+      neighbours.forEach((neighbour) => {
+        if (neighbour.colour === ogColour) {
+          recurse(neighbour);
+        }
+      });
+    }
+
+    recurse(block);
+    return sameColourNeighbours;
+  }
+
+  reorderColumn(block) {
+    let colours = [];
+    for (let y = 0; y < MAX_Y; y++) {
+      if (this.grid[block.x][y].colour !== WHITE) {
+        colours.push(this.grid[block.x][y].colour);
+      }
+    }
+    for (let y = 0; y < MAX_Y; y++) {
+      if (y < colours.length) {
+        this.grid[block.x][y].colour = colours[y];
+      } else {
+        this.removeBlock(this.grid[block.x][y]);
+      }
+    }
+  }
+
+  render(grid = document.querySelector("#gridEl")) {
     let el = grid.cloneNode(false);
     grid.parentNode.replaceChild(el, grid);
     for (let x = 0; x < MAX_X; x++) {
-      const id = 'col_' + x;
-      const colEl = document.createElement('div');
-      colEl.className = 'col';
+      const id = "col_" + x;
+      const colEl = document.createElement("div");
+      colEl.className = "col";
       colEl.id = id;
       el.appendChild(colEl);
 
       for (let y = MAX_Y - 1; y >= 0; y--) {
         const block = this.grid[x][y];
-        const blockEl = document.createElement('div'); 
+        const blockEl = document.createElement("div");
 
-        if(block.clickable) {
-          blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));    
-        }              
+        if (block.clickable) {
+          blockEl.addEventListener("click", (evt) =>
+            this.blockClicked(evt, block)
+          );
+        }
 
         colEl.appendChild(blockEl);
         this.redrawBlock(blockEl, block);
@@ -76,13 +132,13 @@ class BlockGrid {
     return this;
   }
 
-  blockClicked (e, block) {
-    /*
-    what happens on each block click before you re-render?
-    what happens to each column of blocks?
-    */
+  blockClicked(e, block) {
+    let sameColourNeighbours = this.getSameColourNeighbours(block);
+    sameColourNeighbours.forEach((sameColourNeighbour) => {
+      this.reorderColumn(sameColourNeighbour);
+    });
     this.render();
   }
 }
 
-window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
+window.addEventListener("DOMContentLoaded", () => new BlockGrid().render());
--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,70 @@
 const { expect } = chai;
-describe("BlockGrid", function() {
+
+describe("BlockGrid", function () {
   "use strict";
   const blockGrid = new BlockGrid();
 
+  let block =
+    blockGrid.grid[Math.floor(Math.random() * MAX_X)][
+      Math.floor(Math.random() * MAX_Y)
+    ];
+
   describe("clearBlock", () => {
-    it('should turn the block white and make it not clickable');
-    it('should be idempotent');
+    it("should turn the block white", () => {
+      expect(block.colour.toString()).not.to.equal("white");
+      blockGrid.clearBlock(block);
+      expect(block.colour.toString()).to.equal("white");
+    });
+
+    it("should be idempotent", () => {
+      let numberOfCalls = Math.floor(Math.random() * 5);
+      for (let i = 0; i < numberOfCalls; i++) {
+        blockGrid.clearBlock(block);
+      }
+      expect(block.colour.toString()).to.equal("white");
+    });
   });
 
-  describe('removeBlock', () => {
-    it('is idempotent');
-    it('makes the block not clickbale');
-    it('turns the block white');
+  describe("removeBlock", () => {
+    it("makes the block not clickable", () => {
+      expect(block.clickable).to.be.true;
+      blockGrid.removeBlock(block);
+      expect(block.clickable).to.be.false;
+    });
+    it("is idempotent", () => {
+      let numberOfCalls = Math.floor(Math.random() * 5);
+      for (let i = 0; i < numberOfCalls; i++) {
+        blockGrid.removeBlock(block);
+      }
+      expect(block.clickable).to.be.false;
+    });
   });
 
-  describe('getNeighbours', () =>{
-    it('returns the neighrbours to either side and above');
-    it('returns correct results for corner blocks');
-    it('returns correct results for blocks on the edges');
-  });
+  describe("getNeighbours", () => {
+    it("returns the neighbours to either side and above", () => {
+      const centralBlock = blockGrid.grid[5][5];
+      const neighbours = [
+        blockGrid.grid[5][6],
+        blockGrid.grid[5][4],
+        blockGrid.grid[4][5],
+        blockGrid.grid[6][5],
+      ];
+      expect(blockGrid.getNeighbours(centralBlock)).to.have.members(neighbours);
+    });
 
-  describe('reorderColumn', () => {
-    /* Why would we need this function? Can you fill the test in and use it? */
-    it('should ensure that remaining blocks are at the bottom');
+    it("returns correct results for corner blocks", () => {
+      const cornerBlock = blockGrid.grid[0][0];
+      const neighbours = [blockGrid.grid[1][0], blockGrid.grid[0][1]];
+      expect(blockGrid.getNeighbours(cornerBlock)).to.have.members(neighbours);
+    });
+    it("returns correct results for blocks on the edges", () => {
+      const edgeBlock = blockGrid.grid[5][0];
+      const neighbours = [
+        blockGrid.grid[6][0],
+        blockGrid.grid[4][0],
+        blockGrid.grid[5][1],
+      ];
+      expect(blockGrid.getNeighbours(edgeBlock)).to.have.members(neighbours);
+    });
   });
 });
